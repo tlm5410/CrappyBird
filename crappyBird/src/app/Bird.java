93320fa2fc64af0f70a52b46d48c9f52d67b4ca5
tlm5410 <tlm5410@psu.edu>
2014-11-20 13:52:21 -0500

    Updated bird method to have image and move y pos and updated game panel for bird movement

--- crappyBird/src/app/Bird.java
+++ crappyBird/src/app/Bird.java
@@ -1,14 +1,21 @@
 package app;
 
+import java.awt.Image;
+import java.awt.Toolkit;
+
 public class Bird {
-    int x, y, diameter, speed;
+    int x;
+    int y; 
+    int diameter;
+    int speed;
+    Image birdImg;
     
     public Bird(){
         
     }
     
-    public void moveBird(){
-        
+    public void moveBird(int y){
+        y= y;
     }
     
     public void jump(){
@@ -18,4 +25,14 @@ public class Bird {
     public void reset(){
         
     }
+    
+    public Image getImage(){
+        return birdImg;
+    }
+    
+    public void ChangeImage(String newImage, int height, int width){
+        birdImg= Toolkit.getDefaultToolkit().getImage(newImage).getScaledInstance(width, height, Image.SCALE_DEFAULT);
+        
+    }
+    
 }
--- crappyBird/src/app/GamePanel.java
+++ crappyBird/src/app/GamePanel.java
@@ -1,5 +1,9 @@
 package app;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
 import javax.swing.*;
 
 public class GamePanel extends JPanel{
@@ -28,4 +32,32 @@ public class GamePanel extends JPanel{
    public void paintComponent(){
        
    }
+   
+   public void keyPressed(KeyEvent evt) {
+        int kk = evt.getKeyCode();
+        if(kk ==  KeyEvent.VK_SPACE) {crappyBird.moveBird(crappyBird.y+50);}
+       // Rectangle playerA = new Rectangle(Player.getXpos(),Player.getYpos(),100,100);
+        //Rectangle playerB= new Rectangle(car.getXpos(), car.getYpos(), 50, 500);
+	//playerPiece.setBounds(playerA);
+        
+        /*if(playerA.isEmpty()){
+            clock.stop();
+            System.out.println("LOSER");
+        }
+                **/
+        /*if(finishLine.getBounds().contains(Player.xPos+45, Player.yPos+45)){
+                clock.stop();
+                background.setVisible(false);
+                remove(background);
+                JLabel winner = new JLabel("YOU WON!!");
+                winner.setHorizontalAlignment(JLabel.CENTER);
+                winner.setFont(new Font("Serif", Font.PLAIN, 20));
+                winner.setOpaque(true);
+                winner.setBackground(Color.GREEN);
+                add(winner);
+                winner.setVisible(true);
+                //Player.ChangeImage("images/blood.png", 100, 100); 
+        */
+   }
 }
+   
